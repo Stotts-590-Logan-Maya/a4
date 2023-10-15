bf2b04390e8aff55c14e9f28e9c817e49b0f118e
Maya Agnihotri <59749900+mayaagni@users.noreply.github.com>
2023-10-15 18:04:54 -0400

    Update a4.erl

--- a4.erl
+++ a4.erl
@@ -5,26 +5,24 @@ get_numData() ->
         {ok, Num} = io:read("Enter a number: "),
         io:format("The number you entered is: ~w~n", [Num]),
      Num.
+
 factorial(N) -> N * factorial(N - 1).
 
 simple_program_one() ->
-  Num = get_numData(),
-   
+   Num = get_numData(),
    if 
       not Is_Integer(num) ->
-   io:format("not an integer~n");
+   	io:format("not an integer~n");
       Num == 0 -> 
          io:format("0~n"); 
       Num < 0 ->
-   temp = math:pow(abs(Num), 7),
-   io:format("~n", [Result]);
+   	temp = math:pow(abs(Num), 7),
+   	io:format("~n", [Result]);
+      Num rem 7 == 0 ->
+        temp = math:pow(Num, 1/5),
+        io:format("~n", [temp]);
       true -> 
-         if
-          Num rem 7 == 0 ->
-            temp = math:pow(Num, 1/5),
-            io:format("~n", [temp]);
-          true ->
-            temp = factorial(Num),
-            io:format("~n", [temp]);
-    	end
-   end.
+        temp = factorial(Num),
+        io:format("~n", [temp]);
+   
+end.
