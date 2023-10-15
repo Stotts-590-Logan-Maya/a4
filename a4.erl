788ef749376eed4572ce5d05c33af7635d96b639
Logan Stach <loganjeffreystach@gmail.com>
2023-10-14 21:45:21 -0400

    made simple_program_one() function

--- a4.erl
+++ a4.erl
@@ -1,5 +1,17 @@
 -module(a4).
--export([get_numData/0]).
+-export([simple_program_one/0]).
+
 get_numData() ->
         {ok, Num} = io:read("Enter a number: "),
-        io:format("The number you entered is: ~w~n", [Num]).
+        io:format("The number you entered is: ~w~n", [Num]),
+	   Num.
+
+simple_program_one() ->
+Num = get_numData(),
+   
+   if 
+      Num == 2 -> 
+         io:fwrite("True"); 
+      true -> 
+         io:fwrite("False") 
+   end.
